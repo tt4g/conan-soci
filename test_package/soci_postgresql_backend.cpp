cf08e0f3d9431666bbe877df6a11c025b95b4c70
tt4g <mainlinett@gmail.com>
2019-01-13 22:35:05 +0900

    Don't connect to server in tests

--- test_package/soci_postgresql_backend.cpp
+++ test_package/soci_postgresql_backend.cpp
@@ -1,21 +1,18 @@
 #include <soci/soci.h>
 #include <soci/postgresql/soci-postgresql.h>
 
-#include <stdexcept>
-
 int main() {
-    soci::session sql(soci::postgresql,
-                      "user='conan_soci_user' password='conan_soci_pass'"
-                      " host='127.0.0.1' port=5432 dbname='conan_soci_test'"
-                      " application_name='conan_soci' connect_timeout=10"
-                      " options='-c client_encoding=utf8'");
-
-    int count = 0;
-    sql << "select 1 as one", soci::into(count);
-
-    if (count != 1) {
-        throw std::runtime_error("count not equal to 1");
+    try {
+        soci::session sql(soci::postgresql,
+                          "user='conan_soci_user' password='conan_soci_pass'"
+                          " host='127.0.0.1' port=5432 dbname='conan_soci_test'"
+                          " application_name='conan_soci' connect_timeout=10"
+                          " options='-c client_encoding=utf8'");
+    } catch (const soci::soci_error &ex) {
+        // don't connect server in tests.
+        // pass if compile succeeded.
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
