7d7b0a98869b4bef6ef405a9c9dbe7c902e6bdb9
tt4g <mainlinett@gmail.com>
2019-07-15 09:44:18 +0900

    Polish test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,35 +1,12 @@
-import os
-
 from conans import ConanFile, CMake, tools, RunEnvironment
 
 
 class SociTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    # TODO: Remove requires.
-    requires = (
-        ("libpq/9.6.9@bincrafters/stable"), # SOCI PostgreSQL backend
-        ("mysql-connector-c/6.1.11@bincrafters/stable"), # SOCI MySQL backend
-        ("OpenSSL/1.0.2p@conan/stable"), # dependency: libpq, mysql-connector-c
-        ("zlib/1.2.11@conan/stable") # dependency: libpq, mysql-connector-c
-    )
-    # TODO: remove backend options, dependency options
     default_options = {
-        "SOCI:shared": False,
-        # backend options
         "SOCI:with_postgresql": True,
         "SOCI:with_mysql": True,
-
-        # dependency options
-        "libpq:shared": False,
-        "libpq:with_openssl": True,
-        "libpq:with_zlib": True,
-        "mysql-connector-c:shared": False,
-        "mysql-connector-c:with_ssl": True,
-        "mysql-connector-c:with_zlib": True,
-        "OpenSSL:shared": False,
-        "zlib:shared": False,
-        "zlib:minizip": False
     }
 
     def build(self):
@@ -45,8 +22,8 @@ class SociTestConan(ConanFile):
         self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
-        build_type = self.settings.build_type
-
-        test_command = "ctest --output-on-failure --build-config %s" % (build_type,)
+        test_command = "ctest --output-on-failure --build-config %s" \
+                        % (self.settings.build_type,)
 
         self.run(test_command, run_environment=True)
+
